48bbd507582a64c5cdec7335bdc15395c3c5af4b
runelaenen <rune@laenen.me>
2026-05-12 13:22:28 +0200

    Fix ACL grouping

--- src/Resources/app/administration/src/module/lae-admin-lock/acl/index.js
+++ src/Resources/app/administration/src/module/lae-admin-lock/acl/index.js
@@ -4,9 +4,10 @@
  *   lae_admin_lock.viewer        - can open /sw/settings/lae-admin-lock overview
  *   lae_admin_lock.force_unlock  - can break a foreign lock via the bar / overview
  */
+
 Shopware.Service('privileges').addPrivilegeMappingEntry({
-    category: 'permissions',
-    parent: 'system',
+    category: 'additional_permissions',
+    parent: null,
     key: 'lae_admin_lock',
     roles: {
         viewer: {
